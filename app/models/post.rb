7f0d76ef17f00c883c1c7cde36434ca622742717
Brody Losh <brodylosh@gmail.com>
2022-06-07 11:18:56 -0400

    commit tree b39a4228aa1b4f461d6a10c2d2e559234d1e47a3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :must_have_clickbait
+
+    def must_have_clickbait
+        if title
+            unless title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+                errors.add(:title, "This is an invalid title")
+            end
+           
+       end
+    end
+
 end
